b81eb34f0bac683d6d9eae2b6b8de0f271716033
Meredith Jones <mj19003@gmail.com>
2021-02-07 13:40:26 -0500

    Initial setup added for navbar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Meredith Jones React Portfolio
 
 ***
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return ( 
+       <nav className = "navbar">
+           <h2>Meredith Jones Portfolio</h2>
+           <div className = "links">
+               <a href = "/">Home</a>
+           </div>
+       </nav> 
+     );
+}
+ 
+export default Header;
