62385a932bb92612ec33f48329d35b47ff4ba860
Meredith Jones <mj19003@gmail.com>
2021-02-09 21:32:20 -0500

    Cleaned up About Me Page

--- src/Components/Jumbotron/Jumbo.jsx
+++ src/Components/Jumbotron/Jumbo.jsx
@@ -8,8 +8,6 @@ const Jumbo = (props) => {
       <p className={props.messagecss}>{props.message}</p>
       <hr className="my-4" />
       <p>{props.text}</p>
-      <a id={props.centered} className="btn btn-success btn-lg" href={props.href1} role="button">{props.button1}</a>
-      <a id={props.hide} className="btn btn-primary btn-lg" href={props.href2} target="_blank" rel="noopener noreferrer" role="button">{props.button2}</a>
     </div>
   )
 }
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,21 +3,14 @@ import Hero from "../Components/Hero/hero";
 import Jumbo from "../Components/Jumbotron/Jumbo";
 
 const About  = () => {
-
     return (
         <div>
-            <Hero backgroundImage="">
+            <Hero backgroundImage="public/Assets/Images/background.jpg">
             </Hero>
             <Jumbo
             titleName="display-4"
             name="Meredith Jones"
             message=""
-            text="Please refer to the buttons below to access my online resume and portfolio page"
-            href1="/"
-            href2="public/Assets/Resume/meredith_jones_resume .pdf"
-            button1="See my projects"
-            button2="Online Resume"
-            messagecss="lead"
             >
             </Jumbo>
         </div>
